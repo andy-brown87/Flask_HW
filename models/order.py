9e8ac39c4561926309af8168f858e388326d9efa
Andrew Brown <andy_zero@hotmail.com>
2023-09-05 14:57:46 +0100

    populate class

---
+++ models/order.py
@@ -0,0 +1,6 @@
+class Order:
+    def __init__(self, input_customer_name, input_order_date, input_quantity):
+        self.input_customer_name=input_customer_name
+        self.input_order_date=input_order_date
+        self.input_quantity=input_quantity
+
